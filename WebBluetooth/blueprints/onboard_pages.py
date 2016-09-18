478d807a796946b6d869479edd35b6b89da7ec23
Nathan Meadows <nathan.medz@gmail.com>
2016-09-18 04:23:28 -0400

    Much down, push notifications to go

--- WebBluetooth/blueprints/onboard_pages.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask ,Blueprint, render_template
-
-onboard_pages = Blueprint('onboard_pages', __name__, template_folder='templates')
-
-
-@onboard_pages.route("/<route_id>")
-def test(route_id):
-    return render_template('onboard.html')
--- WebBluetooth/main.py
+++ WebBluetooth/main.py
@@ -1,19 +1,39 @@
 from flask import Flask
-from blueprints.stop_pages import stop_pages
-from blueprints.ble_pages import ble_pages
-from blueprints.onboard_pages import onboard_pages
+
+from flask import render_template, jsonify
+from helpers import helpers
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-app.register_blueprint(stop_pages, url_prefix='/stop')
-app.register_blueprint(ble_pages, url_prefix='/ble')
-app.register_blueprint(onboard_pages, url_prefix='/onboard')
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def test():
+    stop_num = "2"
+    today = datetime.now() - timedelta(hours=4)
+    routes = helpers.get_all_routes()
+    stops = helpers.get_all_stops()
+    for stop, times in stops.iteritems():
+        stop_times = times['stop_times'].get('stop_' + stop_num)
+        stop_times = [today.replace(hour=datetime.strptime(stop_time, '%I:%M%p').hour, minute=datetime.strptime(stop_time, '%I:%M%p').minute, second=0) for stop_time in stop_times]
+        if filter(lambda x: x > today, stop_times):
+            next = filter(lambda x: x > today, stop_times)[0]
+            stop_times = filter(lambda x : x > next, stop_times)
+        else:
+            next = stop_times[0]
+            stop_times = stop_times[1:]
+        routes[stop.strip('route_')]['stops'] = {'all': stop_times, 'next': next}
+
+    return render_template('stop.html', routes=routes)
+
+
+@app.route("/ajax/stops/<route_num>")
+def stops(route_num):
+    stops = helpers.get_route_stops(route_num)
+    stop_names = stops.get('stop_names')
+    return jsonify(**stop_names)
 
 if __name__ == "__main__":
     app.run()
