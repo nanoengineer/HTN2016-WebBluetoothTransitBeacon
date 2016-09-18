5f589468ab0f6e8f3f252376fda55a5351403367
Nathan Meadows <nathan.medz@gmail.com>
2016-09-17 21:44:07 -0400

    Things work now! kinda...

--- WebBluetooth/blueprints/stop_pages.py
+++ WebBluetooth/blueprints/stop_pages.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from helpers import helpers
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 stop_pages = Blueprint('stop_pages', __name__, template_folder='templates')
@@ -9,20 +9,20 @@ stop_pages = Blueprint('stop_pages', __name__, template_folder='templates')
 
 @stop_pages.route("/")
 def test():
-    stop_num = "5"
-    stop_dict = {}
-    today = datetime.now()
+    stop_num = "3"
+    today = datetime.now() - timedelta(hours=4)
     routes = helpers.get_all_routes()
     stops = helpers.get_all_stops()
-
     for stop, times in stops.iteritems():
-        stop_times = [times['stop_times'].get('stop_' + stop_num)]
-        # for stop_time in stop_times:
-        #     datetime.strptime(stop_time, '%I:%M%p')
+        stop_times = times['stop_times'].get('stop_' + stop_num)
+        stop_times = [today.replace(hour=datetime.strptime(stop_time, '%I:%M%p').hour, minute=datetime.strptime(stop_time, '%I:%M%p').minute, second=0) for stop_time in stop_times]
+        if filter(lambda x: x > today, stop_times):
+            next = filter(lambda x: x > today, stop_times)[0]
+            stop_times = filter(lambda x : x > next, stop_times)
+        else:
+            next = stop_times[0]
+            stop_times = stop_times[1:]
+        routes[stop.strip('route_')]['stops'] = {'all': stop_times, 'next': next}
 
-
-        stop_dict[stop.strip('route_')] = {'all': times['stop_times'].get('stop_' + stop_num)}
-
-    # todo dump times based on rec'd stop
     return render_template('stop.html', routes=routes)
 
--- WebBluetooth/helpers/helpers.py
+++ WebBluetooth/helpers/helpers.py
@@ -1,21 +1,26 @@
-import pyrebase
+# import pyrebase
+# import requests
+# config = {
+#     'apiKey': "AIzaSyBfyz3Z_dqNHK7dn0JJT4GwqXP1R_xizWo",
+#     'authDomain': "hackthenorth16-1532.firebaseapp.com",
+#     'databaseURL': "https://hackthenorth16-1532.firebaseio.com",
+#     'storageBucket': "hackthenorth16-1532.appspot.com"
+# }
+#
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
 import requests
-config = {
-    'apiKey': "AIzaSyBfyz3Z_dqNHK7dn0JJT4GwqXP1R_xizWo",
-    'authDomain': "hackthenorth16-1532.firebaseapp.com",
-    'databaseURL': "https://hackthenorth16-1532.firebaseio.com",
-    'storageBucket': "hackthenorth16-1532.appspot.com"
-}
-
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
-
 
 def get_all_routes():
-    return db.child("routes").get().val()
+    res = requests.get('https://hackthenorth16-1532.firebaseio.com/routes.json')
+    return res.json()
+    # return db.child("routes").get().val()
 
 def get_all_stops():
-    return db.child("stops").get().val()
+    res = requests.get('https://hackthenorth16-1532.firebaseio.com/stops.json')
+    return res.json()
+    # return db.child("stops").get().val()
+
 
 if __name__ == '__main__':
     import pdb; pdb.set_trace()
